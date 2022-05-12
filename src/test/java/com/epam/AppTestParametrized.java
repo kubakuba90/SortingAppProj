e40b54d8617011d2da8f59ee2135684a3a46d4b8
Jakub M <fotograflubin@gmail.com>
2022-05-12 14:18:38 +0200

    2 Task implementation. With some test. (Before changing from ArrayList to normal array)

--- src/test/java/com/epam/AppTestParametrized.java
+++ src/test/java/com/epam/AppTestParametrized.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 public class AppTestParametrized {
 
     private String numbers;
-    private static ArrayList<Integer> arrayExpected;
+    private static ArrayList<Integer> arrayExpected = new ArrayList<>();
     protected App app = new App();
 
     public AppTestParametrized(String numbers, ArrayList<Integer> arrayExpected) {
@@ -23,25 +23,22 @@ public class AppTestParametrized {
         this.arrayExpected = arrayExpected;
     }
 
-    //List<Integer> listOfSortedParams = new ArrayList<>();
-    int array[] = {0, 1, 1, 2, 3, 4, 6, 10, 16, 99};
 
-//Collections.addAll(arrayExpected, 0, 1, 1, 2, 3, 4, 6, 10, 16, 99)
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
+        Collections.addAll(arrayExpected, 0, 1, 1, 2, 3, 4, 6, 10, 16, 99);
         return Arrays.asList(new Object[][]{
-                {"4 1 3 6 99 0 1 2 16 10", arrayExpected.addAll() }
-               // {"66 77 33 111 99 22 1 26 16 100", Collections.addAll(arrayExpected, 1, 16, 22, 26, 33, 66, 77, 99, 100, 111)}
+                {"4 1 3 6 99 0 1 2 16 10", arrayExpected}
         });
     }
+
+
+
 
     @Test
     public void testTenArguments() {
         assertEquals(app.sortApp(numbers), arrayExpected);
     }
 
-    // class MathUtils{
-    // public static int add(int a, int b) {
-    //      return a+b;
-    // }
+
 }
